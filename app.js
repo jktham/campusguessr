2c1dd58013fda89fabaddac5fbb4de22b39d4c62
jktham-sf <jonas.tham@gmx.ch>
2024-10-12 00:16:14 +0200

    add sophies panorama view

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const fs = require('fs');
+const path = require('path');
+
 const app = express()
 const port = process.env.PORT || 80
 
@@ -10,9 +13,30 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
 	res.sendFile(__dirname + "/public/home.html");
 })
-app.get("/home", (req, res) => {
-	res.sendFile(__dirname + "/public/game.html");
+
+app.get("/game", (req, res) => {
+	res.sendFile(__dirname + "/public/panorama/index.html");
 })
+
+// Route to fetch the list of images from the imgs folder
+app.get('/images', (req, res) => {
+    const imgFolder = path.join(__dirname, 'imgs');
+    fs.readdir(imgFolder, (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'Unable to fetch images' });
+        }
+        // Filter and return only images (jpg, png, gif)
+        const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file));
+        res.json(images);
+    });
+});
+
+// Serve images from the imgs folder
+app.get('/imgs/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const imgPath = path.join(__dirname, 'imgs', filename);
+    res.sendFile(imgPath);
+});
 
 app.listen(port, () => {
 	console.log(`app listening on port ${port}`)
