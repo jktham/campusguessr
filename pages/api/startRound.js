7ce15c36168eecd5aa6a40d7bf209c2dddce5a56
jktham-sf <jonas.tham@gmx.ch>
2024-10-12 22:17:24 +0200

    get buildings and floors

--- pages/api/startRound.js
+++ pages/api/startRound.js
@@ -1,8 +1,13 @@
 import { pool } from "../../utils/db.js"
 
 export default async function handler(req, res) {
+	let mode = {
+		buildings: req.body.buildings || ["HG"],
+		floors: req.body.floors || ["all"]
+	}
+
 	let round = {
-		image_id: 0
+		image_id: 0 // pick random image
 	}
 
 	res.status(200).json(round);
--- pages/test.jsx
+++ pages/test.jsx
@@ -14,6 +14,8 @@ export default function Page() {
 			<button onClick={submitGuess}>Submit guess</button>
 			<button onClick={createUser}>Create user</button>
 			<button onClick={startRound}>Start round</button>
+			<button onClick={getBuildings}>get buildings</button>
+			<button onClick={getFloors}>get floors</button>
 		</div>
 	)
 	
@@ -59,11 +61,39 @@ async function createUser() {
 }
 
 async function startRound() {
+	let mode = {
+		buildings: prompt("buildings"),
+		floors: prompt("floors")
+	}
+
 	let res = await fetch(window.location.origin + "/api/startRound", {
+		method: "POST",
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(mode)
+	});
+	console.log(res);
+
+	res = await res.json().catch((e) => console.error(e));
+	console.log(res);
+}
+
+async function getBuildings() {
+	let res = await fetch(window.location.origin + "/api/getBuildings", {
 		method: "GET"
 	});
 	console.log(res);
 
+	res = await res.json().catch((e) => console.error(e));
+	console.log(res);
+}
+
+async function getFloors() {
+	let building = prompt("building")
+	let res = await fetch(window.location.origin + `/api/getFloors?building=${building}`, {
+		method: "GET"
+	});
+	console.log(res);
+	
 	res = await res.json().catch((e) => console.error(e));
 	console.log(res);
 }
