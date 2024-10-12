eb74fb177da53a891648ad80b84b6264849b7993
mashen <mashen@student.ethz.ch>
2024-10-13 01:40:07 +0200

    Smol change

--- pages/api/util_convert.js
+++ pages/api/util_convert.js
@@ -7,10 +7,10 @@ export default async function handler(req, res) {
 	try {
 		const result = await pool.query(
 			`SELECT floor FROM floor_index WHERE value = ${floorID} AND building = '${building}'`);
-		res.json(result.rows);
+		res.json(result.rows[0].floor);
 	} catch (err) {
 		console.error(err);
 		res.status(500).send('Internal Server Error');
 	}
-	
+
 }
