e53ec59cd8190290b763c011f3dbc39954d7df8b
mashen <mashen@student.ethz.ch>
2024-10-13 01:36:10 +0200

    ADDED util

---
+++ pages/api/util_convert.js
@@ -0,0 +1,16 @@
+import { pool } from "../../utils/db.js"
+
+export default async function handler(req, res) {
+	let floorID = req.body.floorID;
+	let building = req.body.building;
+
+	try {
+		const result = await pool.query(
+			`SELECT floor FROM floor_index WHERE value = ${floorID} AND building = '${building}'`);
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send('Internal Server Error');
+	}
+	
+}
