f67c857094ec5275300e660df89feabe872f03e4
jktham-sf <jonas.tham@gmx.ch>
2024-10-13 11:09:15 +0200

    fix random image 0

--- pages/api/startRound.js
+++ pages/api/startRound.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
 	try {
 		let imageMapRes = await pool.query(`SELECT * FROM image_map WHERE building = '${mode.buildings}'`);
-		let randomImage = imageMapRes.rows[Math.floor(Math.random()*imageMapRes.rowCount)]
+		let randomImage = imageMapRes.rows[Math.floor(Math.random()*(imageMapRes.rowCount - 1)) + 1]
 
 		let imageRes = await pool.query(`SELECT * FROM image WHERE image_id = '${randomImage.image_id}'`);
 		// let mapRes = await pool.query(`SELECT * FROM maps WHERE map_id = '${imageRes.rows[0].map_id}'`); // we dont actually need this here
